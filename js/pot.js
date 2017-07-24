ffae56ec4ca6f4d6e22e22a8821e29158a355253
pookage <pookagehayes@gmail.com>
2017-07-24 17:45:36 +0100

    added soil to the pot

--- js/pot.js
+++ js/pot.js
@@ -11,30 +11,48 @@ AFRAME.registerComponent('pot', {
 	},
 	init: function(){
 
-		const element 	= this.el;
-		const defaults	= this.data;
-		setupPot();
+		const potElement 	= this.el;
+		const defaults		= this.data;
+		const height 		= 0.75;
+		const topRadius 	= 0.45;
+		const bottomRadius	= 0.30;
+		const position 		= potElement.getAttribute("position");
 
+		setupPot();
+		setupSoil();
 
 		function setupPot(){
 
-			const height 			= 0.75;
-			const topRadius 		= 0.45;
-			const bottomRadius		= 0.30;
-			const position 			= element.getAttribute("position");
-			const color 			= defaults.color;
+			const colour 			= defaults.color;
 			const geometry 			= `primitive: cone; height: ${height}; radiusTop: ${topRadius}; radiusBottom: ${bottomRadius};`;
-			const material 			= `metalness: 0; roughness: 1; color: ${color}`;
+			const material 			= `metalness: 0.1; roughness: 1; color: ${colour}`;
 			const offsetPosition 	= `${position.x} ${position.y + ((height * defaults.size) / 2)} ${position.z}`;
 
-			setAttributes(element, {
+			setAttributes(potElement, {
+				geometry: geometry,
+				material: material,
+				position: offsetPosition,
+				scale: `${defaults.size} ${defaults.size} ${defaults.size}`
+			});
+		}//setupPot
+		function setupSoil(){
+
+			const soilElement 		= document.createElement("a-entity");
+			const colour 			= "#382c1d";
+			const geometry 			= `primitive: cone; height: ${height}; radiusTop: ${topRadius * 0.9}; radiusBottom: ${bottomRadius * 0.8};`;
+			const material 			= `metalness: 0; roughness: 1; color: ${colour}`;
+			const offsetPosition 	= `0 ${0.01 * defaults.size} 0`;
+
+			setAttributes(soilElement, {
 				geometry: geometry,
 				material: material,
 				position: offsetPosition,
 				scale: `${defaults.size} ${defaults.size} ${defaults.size}`
 			});
 
-		}//setupPot
+			potElement.appendChild(soilElement);
+
+		}//setupSoil
 	},
 	update: function () {},
 	tick: function () {},
---
+++ js/seed.js
@@ -0,0 +1,13 @@
+AFRAME.registerComponent('seed', {
+	schema: {},
+	init: function(){
+	},
+	update: function () {
+		const element 	= this.el;
+		const defaults 	= this.data;
+	},
+	tick: function () {},
+	remove: function () {},
+	pause: function () {},
+	play: function () {}
+});
